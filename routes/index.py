f8583bd67e57e74dd57ac5f53911f2a41f4dd01d
suzumiya <jrgwyz@163.com>
2017-05-19 23:25:58 +0800

    首页跳转变更

--- routes/index.py
+++ routes/index.py
@@ -6,5 +6,7 @@ main = Blueprint('index', __name__)
 
 @main.route('/')
 def index():
+    if current_user() is not None:
+        return redirect(url_for('user.dashboard'))
     return redirect(url_for('demo.index'))
 
