4d7318e27e420b70ca6c4bc321c7a10536d74775
suzumiya <jrgwyz@163.com>
2017-05-19 20:41:54 +0800

    路径变更

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-ln -s /var/www/md_resume/config/supervisor.conf /etc/supervisor/conf.d/md_resume.conf
+ln -s /var/www/md-resume/config/supervisor.conf /etc/supervisor/conf.d/md-resume.conf
 
-ln -s /var/www/md_resume/config/nginx.conf /etc/nginx/sites-enabled/md_resume
+ln -s /var/www/md-resume/config/nginx.conf /etc/nginx/sites-enabled/md-resume
 
 pip3 install -r requirements.txt
